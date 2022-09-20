46f09fdf95f12926a8a623ec8a7d3bb4dbd4b5c4
hwang-yeongil <jyhwangy4k@gmail.com>
2022-09-20 16:40:54 +0900

    commit

---
+++ src/main/java/org/zeroone/service/BoardServiceImpl.java
@@ -0,0 +1,70 @@
+package org.zeroone.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.zeroone.domain.BoardVO;
+import org.zeroone.domain.Criteria;
+import org.zeroone.mapper.BoardMapper;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Service
+//@AllArgsConstructor
+public class BoardServiceImpl implements BoardService {
+
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper mapper;
+	
+	@Override
+	public int getTotal(Criteria cri) {
+		// TODO Auto-generated method stub
+		log.info("get total count");
+		return mapper.getTotalCount(cri);
+	}
+	@Override
+	public void register(BoardVO board) {
+		// TODO Auto-generated method stub
+		log.info("register__________"+board);
+		mapper.insertSelectKey(board);
+		
+	}
+
+	@Override
+	public BoardVO get(Long bno) {
+		// TODO Auto-generated method stub
+		log.info("get____" + bno);
+		return mapper.read(bno);
+	}
+
+	@Override
+	public boolean modify(BoardVO board) {
+		// TODO Auto-generated method stub
+		log.info("modify_____"+ board);
+		return mapper.update(board) == 1;
+	}
+
+	@Override
+	public boolean remove(Long bno) {
+		// TODO Auto-generated method stub
+		log.info("remove_____"+ bno);
+		return mapper.delete(bno) == 1;
+	}
+
+	@Override
+	public List<BoardVO> getList(Criteria cri) {
+		 log.info("get List with critheria: " + cri);
+		 return mapper.getListWithPaging(cri);
+	}
+
+//	@Override
+//	public List<BoardVO> getList() {
+//		// TODO Auto-generated method stub
+//		log.info("getList_________");
+//		return mapper.getList();
+//	}
+
+}
