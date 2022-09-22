db85c7db5fddfc0f243047538c498f895526d0ee
hwang-yeongil <71959055+hwang-yeongil@users.noreply.github.com>
2022-09-22 16:29:25 +0900

    Update BoardControllertests.java
    space plus

--- src/test/java/org/zeroone/controller/BoardControllertests.java
+++ src/test/java/org/zeroone/controller/BoardControllertests.java
@@ -31,6 +31,7 @@ public class BoardControllertests {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
 	}
 	
+	
 //	@Test
 //	public void testList() throws Exception{
 //		log.info(mockMvc.perform(MockMvcRequestBuilders.get("/board/list")).andReturn().getModelAndView().getModelMap());
